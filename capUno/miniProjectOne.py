c93bcc3e94605ba88ec0be7e7550d1d77226b4a9
mfpachecog <manuelfpg012@gmail.com>
2025-11-09 23:35:16 -0500

    incomplete file

---
+++ capUno/miniProjectOne.py
@@ -0,0 +1,9 @@
+# simple command line tool for freelance IT consultant. 
+# needs to generate a price quote for a service based on an hourly rate an estimaated number
+# of hours. 
+
+HOURLY_RATE = 135.50
+
+SALES_TAX_RATE = 0.07
+
+print("---Welcome to the II Quote Generator---")
